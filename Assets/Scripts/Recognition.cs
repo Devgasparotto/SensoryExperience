d552a5962f0d37829b3e49fe8f9927800d562768
Devgasparotto <devgasp@gmail.com>
2017-04-03 14:53:20 -0400

    First commit with project and rooms created.

---
+++ Assets/Scripts/Recognition.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Recognition : MonoBehaviour {
+
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>(); //contains words to be recognized and associated actions
+
+    GameObject door1;
+    float door1StartTime;
+    Vector3 door1StartPos;
+    Vector3 door1EndPos;
+    bool door1Activated = false;
+    float duration = 3.0f;
+
+
+
+    void Start()
+    {
+        //Initialize
+        keywords.Add("go there", () =>
+        {
+            MoveDoor1();
+        });
+
+        keywords.Add("I am a monkey", () =>
+        {
+            MoveDoor1();
+        });
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhraseRecognized; //when we recognize a phrase - call this
+        keywordRecognizer.Start();
+        Debug.Log("Initialized");
+        door1 = GameObject.FindWithTag("Door1");
+        door1StartPos = door1.transform.position;
+        door1EndPos = new Vector3(door1.transform.position.x, door1.transform.position.y, -13);
+        
+    }
+
+    void Update()
+    {
+        if (door1Activated)
+        {
+            door1.transform.position = Vector3.Lerp(door1StartPos, door1EndPos, (Time.time - door1StartTime) / duration);
+        }
+    }
+
+    void KeywordRecognizerOnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+
+        if(keywords.TryGetValue(args.text, out keywordAction)) //try and get phrase spoken
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    void MoveDoor1()
+    {
+        Debug.Log("GO THERE has been spoken.");
+        door1StartTime = Time.time;
+        door1Activated = true;
+    }
+
+    void MoveDoor2()
+    {
+        Debug.Log("GO THERE has been spoken.");
+        door1StartTime = Time.time;
+        door1Activated = true;
+    }
+}
---
+++ Assets/Teleporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour {
+
+    public string roomName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider collidingObject)
+    {
+        SceneManager.LoadScene(roomName, LoadSceneMode.Single); //NOTE: decided to remove main scene as well to keep memory use low. Also simplifies logic by not considering state of MainChamber - may want to change this if some event happens everytime we return to scene
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
